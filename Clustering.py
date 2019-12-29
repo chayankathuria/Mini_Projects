f0f60e2b309e3600773aded204d545e21179f8dc
Chayan Kathuria <chayankathuria@gmail.com>
2019-12-29 15:21:18 +0530

    Update Clustering.py
    Added Hierarchical clustering, Hyperparameter tuning

--- Clustering.py
+++ Clustering.py
@@ -54,3 +54,157 @@ for i in range(len(y)):
 
 l1 = ax1.legend([c1, c2], ['0', '1'])
 l2 = ax2.legend([c3, c4], ['0', '1'])
+
+# Heirarchical Clustering
+
+from scipy.cluster.hierarchy import dendrogram, linkage
+import numpy as np
+np.set_printoptions(suppress=True)
+
+# Linkage of datapoints 
+Z = linkage(X, 'ward')
+print(Z)
+
+
+# Plotting the dendrogram
+plt.figure(figsize=(8, 3))
+plt.title('Hierarchical Clustering Dendrogram')
+plt.xlabel('Data point')
+plt.ylabel('Distance')
+dendrogram(Z)
+plt.axhline(y=10000, c='k', ls='--', lw=0.5)
+plt.show()
+
+# Clustering
+from scipy.cluster.hierarchy import fcluster
+
+max_dist = 10000
+hc_labels = fcluster(Z, max_dist, criterion='distance')
+
+# Plotting clustered lables along with actual classes
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+fig.suptitle('Visualizing breast cancer clusters')
+fig.subplots_adjust(top=0.85, wspace=0.5)
+ax1.set_title('Actual Labels')
+ax2.set_title('Hierarchical Clustered Labels')
+
+for i in range(len(y)):
+    if y[i] == 0:
+        c1 = ax1.scatter(bc_pca[i,0], bc_pca[i,1],c='g', marker='.')
+    if y[i] == 1:
+        c2 = ax1.scatter(bc_pca[i,0], bc_pca[i,1],c='r', marker='.')
+        
+    if hc_labels[i] == 1:
+        c3 = ax2.scatter(bc_pca[i,0], bc_pca[i,1],c='g', marker='.')
+    if hc_labels[i] == 2:
+        c4 = ax2.scatter(bc_pca[i,0], bc_pca[i,1],c='r', marker='.')
+
+l1 = ax1.legend([c1, c2], ['0', '1'])
+l2 = ax2.legend([c3, c4], ['1', '2'])
+
+# Clustering Model Evaluation Metrics
+
+# Building 2 Models with K=2 and K=5
+km2 = KMeans(n_clusters=2, random_state=42).fit(X)
+km2_labels = km2.labels_
+
+km5 = KMeans(n_clusters=5, random_state=42).fit(X)
+km5_labels = km5.labels_
+
+# Homogeneity, Completeness and V-measure
+km2_hcv = np.round(metrics.homogeneity_completeness_v_measure(y, km2_labels), 3)
+km5_hcv = np.round(metrics.homogeneity_completeness_v_measure(y, km5_labels), 3)
+
+print('Homogeneity, Completeness, V-measure metrics for num clusters=2: ', km2_hcv)
+print('Homogeneity, Completeness, V-measure metrics for num clusters=5: ', km5_hcv)
+
+# Silhouette Coefficient
+from sklearn import metrics
+
+km2_silc = metrics.silhouette_score(X, km2_labels, metric='euclidean')
+km5_silc = metrics.silhouette_score(X, km5_labels, metric='euclidean')
+
+print('Silhouette Coefficient for num clusters=2: ', km2_silc)
+print('Silhouette Coefficient for num clusters=5: ', km5_silc)
+
+# Calinski-Harabaz Index
+km2_chi = metrics.calinski_harabaz_score(X, km2_labels)
+km5_chi = metrics.calinski_harabaz_score(X, km5_labels)
+
+print('Calinski-Harabaz Index for num clusters=2: ', km2_chi)
+print('Calinski-Harabaz Index for num clusters=5: ', km5_chi)
+
+# Model tuning
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+# prepare datasets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# build default SVM model
+def_svc = SVC(random_state=42)
+def_svc.fit(X_train, y_train)
+
+# predict and evaluate performance
+def_y_pred = def_svc.predict(X_test)
+print('Default Model Score:')
+
+def_svc.score(X_test, y_test)
+
+# Hyperparameter tuning with GridSeacrh
+
+
+from sklearn.model_selection import GridSearchCV
+
+# setting the parameter grid
+grid_parameters = {'kernel': ['linear', 'rbf'], 
+                   'gamma': [1e-3, 1e-4],
+                   'C': [1, 10, 50, 100]}
+
+# perform hyperparameter tuning
+print("# Tuning hyper-parameters for accuracy\n")
+clf = GridSearchCV(SVC(random_state=42), grid_parameters, cv=5, scoring='accuracy')
+clf.fit(X_train, y_train)
+# view accuracy scores for all the models
+print("Grid scores for all the models based on CV:\n")
+means = clf.cv_results_['mean_test_score']
+stds = clf.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+    print("%0.5f (+/-%0.05f) for %r" % (mean, std * 2, params))
+# check out best model performance
+print("\nBest parameters set found on development set:", clf.best_params_)
+print("Best model validation accuracy:", clf.best_score_)
+
+# Evaluating the new model
+
+gs_best = clf.best_estimator_
+tuned_y_pred = gs_best.predict(X_test)
+
+print('\n\nTuned Model Stats:')
+
+gs_best.score(X_test,y_test)
+
+# Tuning with Random Search
+
+import scipy
+from sklearn.model_selection import RandomizedSearchCV
+
+param_grid = {'C': scipy.stats.expon(scale=10), 
+              'gamma': scipy.stats.expon(scale=.1),
+              'kernel': ['rbf', 'linear']}
+
+random_search = RandomizedSearchCV(SVC(random_state=42), param_distributions=param_grid,
+                                   n_iter=50, cv=5)
+random_search.fit(X_train, y_train)
+
+print("Best parameters set found on development set:")
+random_search.best_params_
+
+# Evaluating the random search tuned model
+
+rs_best = random_search.best_estimator_
+rs_y_pred = rs_best.predict(X_test)
+
+rs_best.score(X_test,y_test)
